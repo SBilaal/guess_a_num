cf804966b4495addc9785c521d34a951c4fcff61
Bilaal Salman <mbilaalme@gmail.com>
2022-07-06 22:00:20 +0100

    Turns out I wrote absolute nonsense the other time. Works now.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
 
 def guess(x):
-    gen_number = random.randint(0,x)
-    guessed_number = int(input("Guess the number chosen: "))
+    gen_number = random.randint(1,x)
+    guessed_number = 0
     while guessed_number != gen_number:
+        guessed_number = int(input("Guess the number chosen: "))
         if guessed_number == gen_number:
             print("You got it right!")
         if guessed_number > gen_number:
