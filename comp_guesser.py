9f781e21dadccca2275bdfc3418a471aa400f4c4
Bilaal Salman <mbilaalme@gmail.com>
2022-07-06 22:16:36 +0100

    Made comp_guesser a little more intelligent.

--- comp_guesser.py
+++ comp_guesser.py
@@ -3,11 +3,16 @@ import random
 def comp_guesser(x):
     gen_number = int(input("Guess this number I choose: "))
     guessed_number = 0
+    lower = 1
+    higher = x
+
     while guessed_number != gen_number:
-        guessed_number = random.randint(1,x)
+        guessed_number = random.randint(lower,higher)
         if guessed_number == gen_number:
             print("You got it right!")
         if guessed_number > gen_number:
+            higher = guessed_number
             print("You guessed higher")
         if guessed_number < gen_number:
+            lower = guessed_number
             print("You guessed lower")
